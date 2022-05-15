bdd2654050058ed3dce9d5fc4ebe1a50d4a5c5ea
서민혁 <mahns1201@gmail.com>
2022-05-16 02:42:15 +0900

    [feat] modularization

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,1 +1,11 @@
 // get url & return function
+
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('')
+export class AppController {
+  @Get()
+  home() {
+    return 'Welcome to my Movie API';
+  }
+}

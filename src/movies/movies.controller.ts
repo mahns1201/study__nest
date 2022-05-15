f61a4ed2f2f0f70455b906c85cb4f014561ae8b6
서민혁 <mahns1201@gmail.com>
2022-05-16 02:04:17 +0900

    [feat] services

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,43 +8,47 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies') // 'movies' is basic router
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   // query params
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie made after: ${searchingYear}`;
-  }
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after: ${searchingYear}`;
+  // }
 
   @Get('/:id')
-  getMovieById(@Param('id') movieId: string) {
-    return `This will return a movie ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    console.log(movieData);
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
   remove(@Param('id') movieId: string) {
-    return `this will delete a movie ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   // Put -> 리소스 전체 수정
   // Patch -> 리소스 일부만 수정
   @Patch('/:id')
   Patch(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+    return this.moviesService.update(movieId, updateData);
+    // return {
+    //   updatedMovie: movieId,
+    //   ...updateData,
+    // };
   }
 }

940f6156ce2caa8a0b73ea29d3a31d8f53accb63
서민혁 <mahns1201@gmail.com>
2022-05-19 15:02:07 +0900

    [feat] basic method to using repository

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -3,6 +3,10 @@ import { AuthModule } from 'src/auth/auth.module';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
+// import {TypeormModule} from '@nestjs/typeorm';
+// import {MovieRepository} from './repository/movie.repository.ts'
+// imports: [TypeormModule.forFeature([MovieRepository])]
+
 @Module({
   imports: [AuthModule],
   controllers: [MoviesController],
---
+++ src/movies/repository/movie.repository.ts
@@ -0,0 +1,9 @@
+// import { Repository } from "typeorm";
+// import { Movie } from "../entities/movie.entity";
+
+// @EntityRepository(Movie)
+// export class MovieRepository extends Repository<Movie> {
+//   // contents
+// }
+
+// 위 내용을 service의 constructor에 주입해서 사용한다.

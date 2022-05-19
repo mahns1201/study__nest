de4aad55347a7f434121855d0ed65ea1c5622da1
서민혁 <mahns1201@gmail.com>
2022-05-19 15:43:39 +0900

    [fix] secret by config value

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,15 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt/jwt.strategy';
+import * as config from 'config';
 
+const jwtConfig = config.get('jwt');
+const secret = jwtConfig.secret;
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'SuperMahnsSecreteCode', // jwt 토큰 secret text
+      secret, // jwt 토큰 secret text
       signOptions: {
         expiresIn: 60 * 60,
       },
